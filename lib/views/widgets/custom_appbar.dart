ce656e0ad95d1a2c80946ea8ddc458715179856d
M_Eid <meidhsn@gmail.com>
2026-03-14 15:08:35 +0200

    create app  bar

---
+++ lib/views/widgets/custom_appbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app_hive_bloc/views/widgets/custom_search_icon.dart';
+
+class CustomAppbar extends StatelessWidget {
+  const CustomAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text("Notes", style: TextStyle(fontSize: 28)),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app_hive_bloc/views/widgets/custom_appbar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(children: [SizedBox(height: 50), CustomAppbar()]),
+    );
+  }
+}
