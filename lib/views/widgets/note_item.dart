4b4a10bcd90ef38a70748c9c5da612ac39df4d18
M_Eid <meidhsn@gmail.com>
2026-03-15 13:16:58 +0200

    create edit view

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,49 +1,60 @@
 import "package:flutter/material.dart";
 import "package:font_awesome_flutter/font_awesome_flutter.dart";
+import "package:notes_app_hive_bloc/views/edit_view.dart";
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(24),
-      decoration: BoxDecoration(
-        color: Color(0xffffcc80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              "Flutter Tips",
-              style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => EditView(),
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          color: Color(0xffffcc80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                "Flutter Tips",
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Text(
+                "Build your career",
+                style: TextStyle(
+                  color: Colors.black.withValues(alpha: 0.5),
+                  fontSize: 20,
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: FaIcon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                ),
+              ),
             ),
-            subtitle: Text(
-              "Build your career",
+            Text(
+              "May 21, 2022",
               style: TextStyle(
                 color: Colors.black.withValues(alpha: 0.5),
-                fontSize: 20,
+                fontSize: 16,
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: FaIcon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
-              ),
-            ),
-          ),
-          Text(
-            "May 21, 2022",
-            style: TextStyle(
-              color: Colors.black.withValues(alpha: 0.5),
-              fontSize: 16,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
