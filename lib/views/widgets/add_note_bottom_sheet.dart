d21466180ebd487eea21aa6b6339747c582c959b
M_Eid <meidhsn@gmail.com>
2026-03-15 13:09:23 +0200

    create custom button

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:notes_app_hive_bloc/constants.dart";
+import "package:notes_app_hive_bloc/views/widgets/custom_button.dart";
 import "package:notes_app_hive_bloc/views/widgets/custom_text_field.dart";
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,18 +10,22 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextField(
-            hint: "title",
-          ),
-          SizedBox(height: 16),
-          CustomTextField(
-            hint: "content",
-            maxLines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTextField(
+              hint: "title",
+            ),
+            SizedBox(height: 16),
+            CustomTextField(
+              hint: "content",
+              maxLines: 5,
+            ),
+
+            CustomButton(),
+          ],
+        ),
       ),
     );
   }
