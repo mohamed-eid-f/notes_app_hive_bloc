42cf487484b783c97a90f1c52a909eb03f580dc2
M_Eid <meidhsn@gmail.com>
2026-03-15 13:44:10 +0200

    finish edit view ui

--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -1,10 +1,27 @@
 import "package:flutter/material.dart";
+import "package:notes_app_hive_bloc/views/widgets/custom_appbar.dart";
+import "package:notes_app_hive_bloc/views/widgets/custom_text_field.dart";
 
 class EditViewBody extends StatelessWidget {
   const EditViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          CustomAppbar(title: "Edit Note", icon: Icons.check),
+          SizedBox(height: 16),
+          CustomTextField(hint: "Title"),
+          SizedBox(height: 16),
+          CustomTextField(
+            hint: "Content",
+            maxLines: 5,
+          ),
+        ],
+      ),
+    );
   }
 }
